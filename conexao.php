ee3edcd492a68ea410d8f8432544aae445ee36be
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-02-27 15:45:09 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,8 @@
 <?php
-    // conexao.php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "alunos";
 
-// Criar conexão
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Verificar conexão
-    
+$conn = mysqli_connect($servername, $username, $password, $database);    
 ?>
